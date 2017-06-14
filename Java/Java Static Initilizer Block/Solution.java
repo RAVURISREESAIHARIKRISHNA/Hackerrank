33a40d578e8c9ad75bf26c093832deca5caa3a6f
RAVURI SREE SAI HARI KRISHNA <RAVURISREESAIHARIKRISHNA@users.noreply.github.com>
2017-06-14 12:31:15 +0530

    Create Solution.java

---
+++ Java/Java Static Initilizer Block/Solution.java
@@ -0,0 +1,22 @@
+static int B,H;static boolean flag = true;static{
+Scanner scan = new Scanner(System.in);
+
+B = scan.nextInt();
+
+H = scan.nextInt();
+
+try{   
+
+if(B<=0 || H<=0){
+
+flag = false;
+
+throw new Exception("Breadth and height must be positive");
+
+} 
+
+}catch(Exception e){
+
+System.out.println(e);
+
+}}
