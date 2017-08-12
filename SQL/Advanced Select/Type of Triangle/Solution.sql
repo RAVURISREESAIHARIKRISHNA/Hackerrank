91e86feb07da34b3f5adf162dfeff29a970b75a9
RAVURI SREE SAI HARI KRISHNA <RAVURISREESAIHARIKRISHNA@users.noreply.github.com>
2017-08-12 18:20:48 +0530

    Create Solution.sql

---
+++ SQL/Advanced Select/Type of Triangle/Solution.sql
@@ -0,0 +1,12 @@
+select 
+    case 
+        when A+B>C and B+C>A and C+A>B then
+                                            case
+                                                when A=B and B=C and C=A then 'Equilateral'
+                                                when A=B or B=C or C=A then 'Isosceles'
+                                                when A!=B and B!=C and C!=A then 'Scalene'
+                                             end
+                                             else 'Not A Triangle'
+    end
+from triangles;    
+                                                
